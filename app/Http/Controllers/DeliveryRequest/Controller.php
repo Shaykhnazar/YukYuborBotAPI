c8eb3a661821f50bd1bd43e0b70caa3498514b8d
shynkaaaa <shynkataimasov@gmail.com>
2025-04-17 21:19:05 +0500

    added createDeliveryRequest

---
+++ app/Http/Controllers/DeliveryRequest/Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\DeliveryRequest;
+
+use App\Http\Controllers\Controller as BaseController;
+use App\Http\Requests\DeliveryRequest\CreateRequest;
+use App\Models\DeliveryRequest;
+use App\Service\TelegramUserService;
+use Carbon\CarbonImmutable;
+
+class Controller extends BaseController
+{
+    public function __construct(
+        protected TelegramUserService $userService,
+    )
+    {
+    }
+
+    public function create(CreateRequest $request)
+    {
+        $dto = $request->getDTO();
+        $sendReq = new DeliveryRequest(
+            [
+                'from_location' => $dto->fromLoc,
+                'to_location' => $dto->toLoc,
+                'description' => $dto->desc ?? null,
+                'from_date' => $dto->toDate->toDateString(),
+                'to_date' => $dto->toDate->toDateString(),
+                'price' => $dto->price ?? null,
+                'currency' => $dto->currency ?? null,
+                'user_id' => $this->userService->getUserByTelegramId($request)->id,
+                'status' => 'open',
+            ]
+        );
+        $sendReq->save();
+
+        return response()->json($sendReq);
+    }
+}
---
+++ app/Http/DTO/DeliveryRequest/CreateRequestDTO.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\DTO\DeliveryRequest;
+
+use Carbon\CarbonImmutable;
+
+class CreateRequestDTO
+{
+    public function __construct(
+        public string $fromLoc,
+        public string $toLoc,
+        public ?string $desc,
+        public CarbonImmutable $fromDate,
+        public CarbonImmutable $toDate,
+        public ?int $price,
+        public ?string $currency,
+    ) {}
+
+}
