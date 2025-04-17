eebd2c20b7ae0e47e28687b50af20e91cfddea57
1Mein <119441676+1Mein@users.noreply.github.com>
2025-04-17 19:42:57 +0500

    Profile Reviews

---
+++ app/Models/Review.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reviews';
+
+    protected $guarded = false;
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function owner(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id', 'id');
+    }
+}
