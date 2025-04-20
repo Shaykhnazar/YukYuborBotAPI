1a9137033822a98c0f7bb91d03b19fa3381aff2e
1Mein <119441676+1Mein@users.noreply.github.com>
2025-04-21 00:46:35 +0500

    404 error fix

--- app/Http/Controllers/User/Requests/Controller.php
+++ app/Http/Controllers/User/Requests/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User\Requests;
 
 use App\Http\Controllers\Controller as BaseController;
 use App\Http\Requests\Parcel\IndexRequest;
+use App\Http\Resources\Parcel\IndexRequestResource;
 use App\Service\TelegramUserService;
 
 class Controller extends BaseController
@@ -17,8 +18,24 @@ class Controller extends BaseController
     {
         $filter = $request->getFilter();
         $user = $this->tgService->getUserByTelegramId($request);
+        $delivery = collect();
+        $send = collect();
 
-        $requests = $user->sendRequests;
-        return response()->json($requests);
+        if ($filter !== 'delivery') {
+            $send = $user->sendRequests->map(function ($item) {
+                $item->type = 'send';
+                return $item;
+            });;
+        }
+
+        if ($filter !== 'send') {
+            $delivery = $user->deliveryRequests->map(function ($item) {
+                $item->type = 'delivery';
+                return $item;
+            });;
+        }
+        $requests = $delivery->merge($send)->sortByDesc('created_at')->values();
+
+        return IndexRequestResource::collection($requests);
     }
 }
