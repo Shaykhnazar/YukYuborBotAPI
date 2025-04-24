b80680ad8c23c5bbf14ffa9db6d308297e534404
shynkaaaa <shynkataimasov@gmail.com>
2025-04-24 20:12:32 +0500

    feat: added averageRating counter for user

--- app/Http/Controllers/User/Controller.php
+++ app/Http/Controllers/User/Controller.php
@@ -17,19 +17,24 @@ class Controller extends BaseController
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
         $user = $this->tgService->getUserByTelegramId($request);
+        $averageRating = $user->reviews->avg('rating');
         return response()->json([
             'telegram' => $user->telegramUser,
             'user' => collect($user)->except('telegram_user'),
             'reviews' => IndexResource::collection($user->reviews),
+            'average_rating' => round($averageRating, 2),
         ]);
     }
 
     public function show(Request $request, User $user): \Illuminate\Http\JsonResponse
     {
+        $averageRating = $user->reviews->avg('rating');
+
         return response()->json([
             'telegram' => $user->telegramUser,
             'user' => collect($user)->except('telegram_user'),
             'reviews' => IndexResource::collection($user->reviews),
+            'average_rating' => round($averageRating, 2),
         ]);
     }
 }
