dc7f181e221abb4799a5ff2a8ab7d1e99d720c44
shynkaaaa <shynkataimasov@gmail.com>
2025-05-01 22:10:50 +0500

    fix

--- app/Http/Controllers/User/Requests/Controller.php
+++ app/Http/Controllers/User/Requests/Controller.php
@@ -1,69 +1,94 @@
 <?php
 
-    namespace App\Http\Controllers\User\Requests;
+namespace App\Http\Controllers\User\Requests;
 
-    use App\Http\Controllers\Controller as BaseController;
-    use App\Http\Requests\Parcel\IndexRequest;
-    use App\Http\Resources\Parcel\IndexRequestResource;
-    use App\Service\TelegramUserService;
+use App\Http\Controllers\Controller as BaseController;
+use App\Http\Requests\Parcel\IndexRequest;
+use App\Http\Resources\Parcel\IndexRequestResource;
+use App\Service\TelegramUserService;
+use App\Models\User;
 
-    class Controller extends BaseController
+class Controller extends BaseController
+{
+    public function __construct(
+        protected TelegramUserService $tgService,
+    )
+    {}
+
+    public function index(IndexRequest $request)
     {
-        public function __construct(
-            protected TelegramUserService $tgService,
-        )
-        {}
+        $filter = $request->getFilter();
+        $user = $this->tgService->getUserByTelegramId($request);
+        $delivery = collect();
+        $send = collect();
 
-        public function index(IndexRequest $request)
-        {
-            $filter = $request->getFilter();
-            $user = $this->tgService->getUserByTelegramId($request);
-            $delivery = collect();
-            $send = collect();
-
-            if ($filter !== 'delivery') {
-                $send = $user->sendRequests->map(function ($item) {
-                    $item->type = 'send';
-                    return $item;
-                });;
-            }
-
-            if ($filter !== 'send') {
-                $delivery = $user->deliveryRequests->map(function ($item) {
-                    $item->type = 'delivery';
-                    return $item;
-                });;
-            }
-            $requests = $delivery->concat($send)->sortByDesc('created_at')->values();
-
-            return IndexRequestResource::collection($requests);
+        if ($filter !== 'delivery') {
+            $send = $user->sendRequests->map(function ($item) {
+                $item->type = 'send';
+                return $item;
+            });;
         }
 
-        public function show(IndexRequest $request, int $id)
-        {
-            $filter = $request->getFilter();
-            $user = $this->tgService->getUserByTelegramId($request);
-
-            $delivery = collect();
-            $send = collect();
-
-            if ($filter !== 'delivery') {
-                $send = $user->sendRequests->where('id', $id)->map(function ($item) {
-                    $item->type = 'send';
-                    return $item;
-                });
-            }
-
-            if ($filter !== 'send') {
-                $delivery = $user->deliveryRequests->where('id', $id)->map(function ($item) {
-                    $item->type = 'delivery';
-                    return $item;
-                });
-            }
-
-            $requests = $delivery->concat($send)->sortByDesc('created_at')->values();
-
-            return IndexRequestResource::collection($requests);
+        if ($filter !== 'send') {
+            $delivery = $user->deliveryRequests->map(function ($item) {
+                $item->type = 'delivery';
+                return $item;
+            });;
         }
+        $requests = $delivery->concat($send)->sortByDesc('created_at')->values();
 
+        return IndexRequestResource::collection($requests);
     }
+
+    public function show(IndexRequest $request, int $id)
+    {
+        $filter = $request->getFilter();
+        $user = $this->tgService->getUserByTelegramId($request);
+
+        $delivery = collect();
+        $send = collect();
+
+        if ($filter !== 'delivery') {
+            $send = $user->sendRequests->where('id', $id)->map(function ($item) {
+                $item->type = 'send';
+                return $item;
+            });
+        }
+
+        if ($filter !== 'send') {
+            $delivery = $user->deliveryRequests->where('id', $id)->map(function ($item) {
+                $item->type = 'delivery';
+                return $item;
+            });
+        }
+
+        $requests = $delivery->concat($send)->sortByDesc('created_at')->values();
+
+        return IndexRequestResource::collection($requests);
+    }
+    public function userRequests(IndexRequest $request, User $user)
+    {
+        $filter = $request->getFilter();
+
+        $delivery = collect();
+        $send = collect();
+
+        if ($filter !== 'delivery') {
+            $send = $user->sendRequests->map(function ($item) {
+                $item->type = 'send';
+                return $item;
+            });
+        }
+
+        if ($filter !== 'send') {
+            $delivery = $user->deliveryRequests->map(function ($item) {
+                $item->type = 'delivery';
+                return $item;
+            });
+        }
+
+        $requests = $delivery->concat($send)->sortByDesc('created_at')->values();
+
+        return IndexRequestResource::collection($requests);
+    }
+}
