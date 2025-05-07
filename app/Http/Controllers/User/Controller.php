a5401d8c6cd6d6ba5896f420fe972c47df86f90b
1Mein <119441676+1Mein@users.noreply.github.com>
2025-05-07 15:19:02 +0500

    add with_us attribute

--- app/Http/Controllers/User/Controller.php
+++ app/Http/Controllers/User/Controller.php
@@ -6,17 +6,27 @@ use App\Http\Controllers\Controller as BaseController;
 use App\Http\Resources\Review\IndexResource;
 use App\Models\User;
 use App\Service\TelegramUserService;
+use Carbon\Carbon;
+use Carbon\Translator;
 use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
+    private string $customLocale = 'ru_custom';
+
     public function __construct(
         protected TelegramUserService $tgService,
-    ) {}
+    ) {
+        Translator::get($this->customLocale)->setTranslations([
+            'ago' => 'С нами уже :time',
+        ]);
+    }
 
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
         $user = $this->tgService->getUserByTelegramId($request);
+        $user['with_us'] = Carbon::parse($user['created_at'])->locale($this->customLocale)->diffForHumans();
+
         $averageRating = $user->reviews->avg('rating');
         return response()->json([
             'telegram' => $user->telegramUser,
@@ -29,6 +39,7 @@ class Controller extends BaseController
     public function show(Request $request, User $user): \Illuminate\Http\JsonResponse
     {
         $averageRating = $user->reviews->avg('rating');
+        $user['with_us'] = Carbon::parse($user['created_at'])->locale($this->customLocale)->diffForHumans();
 
         return response()->json([
             'telegram' => $user->telegramUser,
